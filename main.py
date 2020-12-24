c45505ebae67d1bdf4cc4b4f8b2ef208ece67b85
needleworm <bhban@kakao.com>
2020-12-25 03:29:43 +0900

    고화질 이미지 다운로드 기능 추가

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ https://github.com/needleworm/pixabay_crawling
 
 import sys
 import os
-import pixabay_crawler as pc
 
 # 검색 키워드
 keyword = sys.argv[1]
@@ -19,6 +18,14 @@ numImages = int(sys.argv[2])
 result_dir = sys.argv[3]
 if result_dir not in os.listdir():
     os.mkdir(result_dir)
+
+# (True / False)
+high_resolution = sys.argv[4]
+
+if high_resolution in "True true":
+    import pixabay_2x_crawling as pc
+elif high_resolution in "False false":
+    import pixabay_crawling as pc
 
 # 크롤링을 수행합니다
 pc.crawling(keyword, numImages, result_dir)
--- pixabay_2x_crawling.py
+++ pixabay_2x_crawling.py
@@ -27,14 +27,34 @@ def crawling(keyword, numImages, result_dir):
         image_area = driver.find_element_by_xpath(xpath)
         image_elements = image_area.find_elements_by_tag_name("img")
         for i in range(numImages):
-            image_elements[i].screenshot(result_dir + "/" + str(time.time()) + ".png")
+            x2_image_url = image_elements[i].get_attribute("srcset").split(" ")[2]
+            driver.execute_script("window.open(' ');")
+            new_tab = driver.window_handles[-1]
+            driver.switch_to.window(new_tab)
+            driver.get(x2_image_url)
+            image_element = driver.find_element_by_tag_name("img")
+            image_element.screenshot(result_dir + "/" + str(time.time()) + ".png")
+            driver.close()
+            original_tab = driver.window_handles[0]
+            driver.switch_to.window(original_tab)
+
     # 100장 이상을 요구받은 경우
     else:
         while numImages > 0:
             image_area = driver.find_element_by_xpath(xpath)
             image_elements = image_area.find_elements_by_tag_name("img")
             for i in range(len(image_elements)):
-                image_elements[i].screenshot(result_dir + "/" + str(time.time()) + ".png")
+                x2_image_url = image_elements[i].get_attribute("srcset").split(" ")[2]
+                driver.execute_script("window.open(' ');")
+                new_tab = driver.window_handles[-1]
+                driver.switch_to.window(new_tab)
+                driver.get(x2_image_url)
+                image_element = driver.find_element_by_tag_name("img")
+                image_element.screenshot(result_dir + "/" + str(time.time()) + ".png")
+                driver.close()
+                original_tab = driver.window_handles[0]
+                driver.switch_to.window(original_tab)
+
                 numImages -= 1
                 if i == len(image_elements) - 1:
                     next_button = driver.find_element_by_partial_link_text("다음 페이지")
